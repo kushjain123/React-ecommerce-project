83bf54d6005327938ef8138a3fd018412d662b11
Kushagra Jain <kushagra.jain@Kushagra-Jain-MBP.local>
2022-12-27 01:57:25 +0530

    product reducer added

--- src/Contact.js
+++ src/Contact.js
@@ -32,7 +32,7 @@ const Contact = () => {
     <Wrapper>
       <h2 className="common-heading">Contact page</h2>
 
-      <iframe
+      <iframe   // show google map with location on contact page
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3501.4888699582434!2d77.33027457583769!3d28.645077383510262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfad7e4b26519%3A0x92f01dc5ca938847!2sMega%20Store!5e0!3m2!1sen!2sin!4v1670181495055!5m2!1sen!2sin"
         width="100%"
         height="400"
